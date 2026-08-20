5e640e2fb1e30a545f4579843fe619d7def3e80a
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2026-08-20 12:03:09 +0530

    Created Endpoint to get songs

--- Muse.Api/Controllers/AppController.cs
+++ Muse.Api/Controllers/AppController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Muse.Api.Services;
 
 namespace Muse.Api.Controllers;
 
@@ -6,9 +7,26 @@ namespace Muse.Api.Controllers;
 [Route("[controller]")]
 public class AppController : ControllerBase
 {
+   private readonly ISpotifyService _spotifyService;
+
+   public AppController(ISpotifyService spotifyService)
+   {
+      _spotifyService = spotifyService;
+   }
+   
    [HttpGet]
    public ActionResult<string> Get()
    {
       return Ok("Hello World");
+   }
+
+   [HttpGet("songs")]
+   public async Task<IActionResult> GetSongsFromMovies([FromQuery] string title)
+   {
+      var result = await _spotifyService.GetSoundtrackAsync(title);
+
+      if (result is null) return NotFound("No soundtrack for '{title}'.");
+      
+      return Ok(new { movie = title, album = result.Value.AlbumName, songs = result.Value.Songs });
    }
 }
